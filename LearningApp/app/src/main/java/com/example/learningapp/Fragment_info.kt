f4eba722b01cca336c84fc512c1fe0667c32b564
Lucas_NB\Lucas01 <lucas@thiess.info>
2020-06-12 18:13:42 +0200

    +

--- LearningApp/app/src/main/java/com/example/learningapp/Fragment_info.kt
+++ LearningApp/app/src/main/java/com/example/learningapp/Fragment_info.kt
@@ -3,11 +3,14 @@ package com.example.learningapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.learningapp.databinding.FragmentInfoBinding
 import timber.log.Timber
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -42,23 +45,85 @@ class Fragment_info : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_info,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_info, container, false)
         try {
-            var element = arguments?.let { Fragment_infoArgs.fromBundle(it).resivedLearnigElement.learningElement }
+            var element =
+                arguments?.let { Fragment_infoArgs.fromBundle(it).resivedLearnigElement.learningElement }
 
 
             if (element is Lesson) {
                 lesson = element
-                 binding.textViewHeading.text=lesson.getName()
-                binding.textViewInfo.text=lesson.text
+                binding.textViewHeading.text = lesson.getName()
+                binding.textViewInfo.text = lesson.text
             }
-        }
-        catch (e:Exception){
+            generateMenue()
+        } catch (e: Exception) {
             Timber.i(e)
         }
 
         return binding.root//inflater.inflate(R.layout.fragment_info, container, false)
     }
+
+
+    private fun generateMenue() {
+
+        binding.buttonQuestions.setOnClickListener {
+            genPopupMenue(it)
+        }
+    }
+
+    private fun genPopupMenue(view: View) {
+        var popupMenu: PopupMenu = PopupMenu(requireContext(), view)
+        var itemList: MutableList<String> = mutableListOf()
+        for (i in 0 until lesson.questions.size) { //(lesson in subject.lessons) {
+
+
+            popupMenu.menu.add(1, i, i, lesson.questions[i].getName())
+
+            itemList.add(lesson.questions[i].getName())
+
+        }
+        popupMenu.setOnMenuItemClickListener {
+            onMenuItemClick(it, itemList)
+        }
+
+        popupMenu.show()
+    }
+
+    @Override
+    private fun onMenuItemClick(item: MenuItem, itemList:MutableList<String>):Boolean
+    {
+
+        if(item.itemId<itemList.size){
+            var serilLearningElement=SerilLearningElement()
+
+            for( quest in lesson.questions)
+            {
+                if(quest.getName()==itemList[item.itemId]){
+                    serilLearningElement.learningElement=quest
+
+
+                    var action =FragmentMenueDirections.actionFragmentMenueToFragmentInfo(serilLearningElement)
+
+
+                    findNavController().navigate(action)
+                }
+            }
+
+            return true
+        }
+        return false
+
+    }
+
+
+
+
+
+
+
+
+
 
     companion object {
         /**
