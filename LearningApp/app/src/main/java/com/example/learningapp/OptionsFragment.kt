4218a4646fe824ce05d4e6dda19a698ee6e2b6e2
Lucas_NB\Lucas01 <lucas@thiess.info>
2020-06-23 18:39:23 +0200

    +

--- LearningApp/app/src/main/java/com/example/learningapp/OptionsFragment.kt
+++ LearningApp/app/src/main/java/com/example/learningapp/OptionsFragment.kt
@@ -39,10 +39,7 @@ class OptionsFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_options, container, false)
 
         binding.buttonReset.setOnClickListener {
